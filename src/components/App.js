81b6bc2554b21064970455c5408b715ea57cb29c
RahulR <rash235r@gmail.com>
2018-11-15 14:18:20 +0400

    base files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CommentBox from "./commentBox";
+import CommentList from "./commentList";
+
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
