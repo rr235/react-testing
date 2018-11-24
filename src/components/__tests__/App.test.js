461fe9893f57a65c06cc202f65b45f9436382938
RahulR <rash235r@gmail.com>
2018-11-24 22:26:21 +0400

    check instance of comment list

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,9 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 import CommentBox from "../commentBox";
+import CommentList from "../commentList";
 
 it("shows a comment box", () => {
   const wrapper = shallow(<App />);
 
   expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper.find(CommentList).length).toEqual(1);
 });
