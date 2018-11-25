e876de02903b13c6ea0b2875ca7752cad2e684ad
RahulR <rash235r@gmail.com>
2018-11-25 10:49:17 +0400

    test for comments reducer

---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,17 @@
+import commentsReducer from "reducers/comments";
+import { SAVE_COMMENT } from "actions/types";
+
+it("handles SAVE_COMMENT action", () => {
+  const action = {
+    type: SAVE_COMMENT,
+    payload: "New Comment"
+  };
+
+  const newState = commentsReducer([], action);
+  expect(newState).toEqual(["New Comment"]);
+});
+
+it("handles unknown type action", () => {
+  const newState = commentsReducer([], {});
+  expect(newState).toEqual([]);
+});
